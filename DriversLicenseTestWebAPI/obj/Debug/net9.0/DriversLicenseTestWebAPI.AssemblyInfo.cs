26dd07f8c2f1444e65895a37642653eeaf9813d7
ka1maxa <tevzivar@gmail.com>
2025-06-07 01:02:11 +0400

    .

--- DriversLicenseTestWebAPI/obj/Debug/net9.0/DriversLicenseTestWebAPI.AssemblyInfo.cs
+++ DriversLicenseTestWebAPI/obj/Debug/net9.0/DriversLicenseTestWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DriversLicenseTestWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01b640bec305d66ad6b6908e8a9e1917aefcf232")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63132ee00625530a1e317c64d442551d4a6f3576")]
 [assembly: System.Reflection.AssemblyProductAttribute("DriversLicenseTestWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DriversLicenseTestWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
